7cd482de5c26a835599eb5f86fe34ab9fbee286a
Guilherme Bacan <guilherme.bacan@gmail.com>
2022-09-23 18:00:03 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD/Migrations/20220923204455_cria.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD.Migrations
+{
+    public partial class cria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionario",
+                columns: table => new
+                {
+                    IdFuncionario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RG = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Emissor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CNH = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gestor = table.Column<bool>(type: "bit", nullable: false),
+                    CEP = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Refencia = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionario", x => x.IdFuncionario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionario");
+        }
+    }
+}
---
+++ CRUD/Models/Funcionario.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CRUD.Models
+{
+    [Table("Funcionario")]
+    public class Funcionario
+    {
+        [Column("IdFuncionario")]
+        [Display(Name = "IdFuncionario")]
+        [Key]
+        public int IdFuncionario { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("CPF")]
+        [Display(Name = "CPF")]
+        public string CPF { get; set; }
+
+        [Column("RG")]
+        [Display(Name = "RG")]
+        public string RG { get; set; }
+
+        [Column("Emissor")]
+        [Display(Name = "Orgão emissor do RG")]
+        public string Emissor { get; set; }
+
+        [Column("Titulo")]
+        [Display(Name = "Titulo de elitor")]
+        public string Titulo { get; set; }
+
+        [Column("CNH")]
+        [Display(Name = "CNH")]
+        public string CNH { get; set; }
+
+        [Column("Gestor")]
+        [Display(Name = "Considerar como gestor")]
+        public bool Gestor { get; set; }
+
+        [Column("CEP")]
+        [Display(Name = "CEP")]
+        public string CEP { get; set; }
+
+        [Column("Endereco")]
+        [Display(Name = "Endereço")]
+        public string Logradouro { get; set; }
+
+        [Column("Numero")]
+        [Display(Name = "Numero")]
+        public int Numero { get; set; }
+
+        [Column("Bairro")]
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [Column("Cidade")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Column("Estado")]
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        [Column("Refencia")]
+        [Display(Name = "Ponto de Refencia")]
+        public string Refencia { get; set; }
+
+    }
+}
